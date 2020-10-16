a4bdce66e003b1f00d875fa7f671ed3c34f0853d
Khaildoran <sgermscheid@gmail.com>
2020-10-16 16:46:46 -0500

    defined GET route in server

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,6 +1,7 @@
 const pg = require('pg');
 
 const Pool = pg.Pool;
+//initialize PG pool 
 const pool = new Pool({
     database: 'listToDo',
     host: 'localhost',
@@ -8,11 +9,11 @@ const pool = new Pool({
     max: 10,
     idleTimeoutMillis: 30000
 })
-
+// return successful connect
 pool.on('connect', () => {
     console.log('PostgreSQL Connected');
 })
-
+// return errors from pool connection
 pool.on('error', (error) => {
     console.log('error with PostgresSQL', error);
 })
--- server/routes/router.js
+++ server/routes/router.js
@@ -5,7 +5,9 @@ const pool = require('../modules/pool')
 // POST - CREATE
 app.post('/', (req, res)=>{
     // define query text
+ 
     // add log information to help with troubleshooting
+    
     // define pool request
     //add catch
 })
@@ -13,10 +15,18 @@ app.post('/', (req, res)=>{
 // GET - READ
 app.get('/', (req, res)=>{
     // define query text
+    let queryText = `SELECT * FROM "toDoList";`
     // add log information to help with troubleshooting
+    console.log('/todo/ GET express call started');
     // define pool request
+    pool.query(queryText).then((results)=>{
+        console.log('request successful');
+        res.send(results.rows)
     //add catch
-})
+    }).catch((error)=>{
+        console.log('request failed', error); 
+    });
+});
 
 // PUT - UPDATE
 app.put()('/done/:id', (req, res)=>{
